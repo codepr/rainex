50e9771ff14f07a0e24fced1a7603022a14eaaf5
Andrea Baldan <andrea@duffel.com>
2022-06-05 18:32:19 +0100

    Transitioning to GenServer implementation

--- lib/metex.ex
+++ lib/metex.ex
@@ -12,7 +12,11 @@ defmodule Metex do
       :world
 
   """
-  def hello do
-    :world
+  def temperature_of(cities) do
+    coordinator_pid = spawn(Metex.Coordinator, :loop, [[], Enum.count(cities)])
+    cities |>Enum.each(fn city ->
+        worker_pid = spawn(Metex.Worker, :loop, [])
+        send worker_pid, {coordinator_pid, city}
+        end)
   end
 end
